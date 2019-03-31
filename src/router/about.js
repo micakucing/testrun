a17a163891332a5e8f5a1b700e7f3e4cd85f1a6b
mica neko <micakucing@gmail.com>
2019-03-31 12:40:50 +0700

    l

---
+++ src/router/about.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import { Link } from "react-router-dom";
+
+class about extends Component {
+    componentDidMount() {}
+    render() {
+        return (
+
+            <div>
+
+            <Link to="/">Home</Link>
+            <br />
+ about 
+      </div>
+
+        );
+    }
+}
+export default about;
---
+++ src/router/home.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { Link } from "react-router-dom";
+
+class home extends Component {
+    componentDidMount() {}
+    render() {
+        return (
+
+            <div>
+<Link to="/about">About</Link>
+<br />
+ Aing 
+      </div>
+
+        );
+    }
+}
+export default home;
